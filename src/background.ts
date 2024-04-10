1e81aceb338a70e8106613ce3ee0da438f47462c
zshnb <a857681664@gmail.com>
2024-04-10 11:48:43 +0800

    download video

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,38 @@
 import type {DownloadMessage} from "~types";
 
-chrome.runtime.onMessage.addListener((message: DownloadMessage) => {
+chrome.runtime.onMessage.addListener(async (message: DownloadMessage, sender, sendResponse) => {
   const {videoId, target} = message
   switch (target) {
     case "thumbnail": {
       const downloadUrl = `https://i.ytimg.com/vi/${videoId}/hq720.jpg`
-      chrome.downloads.download({
+      await chrome.downloads.download({
         url: downloadUrl,
         filename: `thumbnail_hq720_${videoId}.jpg`
       })
+      sendResponse('ok')
+      break
+    }
+    case "video": {
+      const response = await fetch('https://co.wuk.sh/api/json', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: encodeURI(`https://www.youtube.com/watch?v=${videoId}`),
+        })
+      })
+      if (response.ok) {
+        const json = await response.json()
+        await chrome.windows.create({
+          url: json.url
+        })
+        sendResponse('ok')
+      } else {
+        sendResponse(undefined)
+      }
+      break
     }
   }
 })
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react"
-import {Button, Flex} from "antd";
+import {Button, Flex, message} from "antd";
 import Paragraph from "antd/es/typography/Paragraph";
 import '~style.css'
 import {DownloadOutlined} from "@ant-design/icons";
@@ -7,6 +7,7 @@ import type {DownloadMessage} from "~types";
 import {youtubeUtil} from "~util/youtubeUtil";
 
 function IndexPopup() {
+  const [messageApi, contextHolder] = message.useMessage();
   const [currentUrl, setCurrentUrl] = useState("");
   useEffect(() => {
     chrome.tabs.query({
@@ -14,24 +15,43 @@ function IndexPopup() {
       active: true,
     }).then(res => {
       setCurrentUrl(res[0].url)
-      console.log(res)
     })
   }, [])
 
+  const {getYouTubeVideoId} = youtubeUtil(currentUrl)
   async function handleDownloadThumbnail() {
-    const {getYouTubeVideoId} = youtubeUtil(currentUrl)
-    await chrome.runtime.sendMessage({
+    const response = await chrome.runtime.sendMessage({
       videoId: getYouTubeVideoId(currentUrl),
       target: 'thumbnail'
     } as DownloadMessage)
+    console.log(`download thumbnail response: ${response}`)
+    if (!response) {
+      messageApi.error({
+        content: 'download thumbnail error, try later.'
+      })
+    }
+  }
+
+  async function handleDownloadVideo() {
+    const response = await chrome.runtime.sendMessage({
+      videoId: getYouTubeVideoId(currentUrl),
+      target: 'video'
+    } as DownloadMessage)
+    console.log(`download video response: ${response}`)
+    if (!response) {
+      messageApi.error({
+        content: 'download video error, try later.'
+      })
+    }
   }
   return (
     <>
+      {contextHolder}
       <Flex gap='middle' className='min-w-[300px] min-h-[300px] p-4'>
         {
           /https:\/\/(www.)?youtube\.com\/.*/.test(currentUrl) ? (
             <>
-              <Button className='flex items-center'><DownloadOutlined/> video</Button>
+              <Button className='flex items-center' onClick={handleDownloadVideo}><DownloadOutlined/> video</Button>
               <Button className='flex items-center' onClick={handleDownloadThumbnail}><DownloadOutlined/> thumbnail</Button>
               <Button className='flex items-center'><DownloadOutlined/> subtitle</Button>
             </>
