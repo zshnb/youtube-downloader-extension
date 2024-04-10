dbe8ce5bd5d8f33f04147db23acdd3d1460c6cb9
zshnb <a857681664@gmail.com>
2024-04-10 10:51:18 +0800

    init

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,36 @@
-import { CountButton } from "~features/count-button"
-
-import "~style.css"
+import {useEffect, useState} from "react"
+import {Button, Flex} from "antd";
+import Paragraph from "antd/es/typography/Paragraph";
+import '~style.css'
+import {DownloadOutlined} from "@ant-design/icons";
 
 function IndexPopup() {
+  const [currentUrl, setCurrentUrl] = useState("");
+  useEffect(() => {
+    chrome.tabs.query({
+      currentWindow: true,
+      active: true,
+    }).then(res => {
+      setCurrentUrl(res[0].url)
+      console.log(res)
+    })
+  }, [])
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
-    </div>
+    <>
+      <Flex gap='middle' className='min-w-[300px] min-h-[300px] p-4'>
+        {
+          /https:\/\/(www.)?youtube\.com\/.*/.test(currentUrl) ? (
+            <>
+              <Button className='flex items-center'><DownloadOutlined/> video</Button>
+              <Button className='flex items-center'><DownloadOutlined/> thumbnail</Button>
+              <Button className='flex items-center'><DownloadOutlined/> subtitle</Button>
+            </>
+          ) : (
+            <Paragraph>No support download</Paragraph>
+          )
+        }
+      </Flex>
+    </>
   )
 }
 
